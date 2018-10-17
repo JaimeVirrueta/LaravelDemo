8f2d451e529b439ae27d7f3a031539f85fdd62a8
Jaime D. Virrueta Fuentes <jaime.virruetaf@gmail.com>
2018-10-16 20:20:51 -0500

    Creación del controlador HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+    * Muestra un formulario para la creación de un elemento en el recurso
+    * @return \Illuminate\Http\Response
+    */
+    public function create(Request $request)
+    {
+
+    }
+}
